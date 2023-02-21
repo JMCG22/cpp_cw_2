f0c91b5a620dea8233d979e2d48c8a672f55fe54
George Coxon <george.coxon@live.co.uk>
2023-02-21 11:24:34 +0000

    Q7

--- Q7/functionLib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/* Approximate formula to find the normal CDF */
-double normCDF(double x);
-
-/* Get the Black-Scholes price for given inputs*/
-double blackScholesCallPrice(double strike, double spot, double volatility, double interestRate, double time);
---
+++ Q7/header.h
@@ -0,0 +1,3 @@
+
+double normalCDF(double x);
+double bs_call_price(double s0, double strike, double vol, double r, double t );
