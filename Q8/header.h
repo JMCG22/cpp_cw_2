0c4c2364164fb152cc192b745808a0ee86dd2f11
George Coxon <george.coxon@live.co.uk>
2023-02-22 14:54:52 +0000

    bisection fix

--- Q8/header.h
+++ Q8/header.h
@@ -2,8 +2,9 @@
 
 #include <stdlib.h>
 #include <iostream >
-using namespace std;
+#include <stdexcept>
 
+using namespace std;
 
 double normalCDF(double x);
 
@@ -11,41 +12,56 @@ template <typename T>
 double interval_bisection(double y_target, double left,
                           double right, double epsilon, T functor, int max_iter = 1000)
 {
-    /*
-    Find the root of a function using the interval bisection method
-    */
 
-    double mid, y_mid;
+    double y_left = functor(left);
+    double y_right = functor(right);
 
-    do
-    {   
-        // calculate the midpoint
-        mid = (right + left) / 2;
+    // check that the function has opposite signs at the endpoints
+    // this is advised for the bisection method
+    if ((y_left - y_target) * (y_right - y_target) > 0)
+    {
+        throw std::invalid_argument("The function has the same sign at the left and right endpoints.");
+    }
+
+    double y_mid = 0.0;
+    double mid = 0.0;
+    int iter = 0;
+
+    // iterate until the midpoint is within the desired tolerance
+    // or the maximum number of iterations is reached
+    while (iter < max_iter)
+    {
+        mid = (left + right) / 2.0;
         y_mid = functor(mid);
 
-
-        if (y_target < y_mid)
+        if (y_mid == y_target || (right - left) / 2.0 < epsilon)
         {
-            // if the target is less than the midpoint, move the right bound
-            right = mid;
+            // given float equality checks, most likely the root is within the tolerance
+            // so return the midpoint
+            return mid;
+        }
+
+        iter++;
+
+        // update the left and right endpoints
+        if ((y_mid - y_target) * (y_left - y_target) > 0)
+        {
+            // the midpoint has the same sign as the left endpoint so update the left endpoint
+            left = mid;
+            y_left = y_mid;
         }
         else
         {
-            //  if the target is greater than the midpoint, move the left bound
-            left = mid;
+            // the midpoint has the same sign as the right endpoint so update the right endpoint
+            right = mid;
+            y_right = y_mid;
         }
-        max_iter--;
-
-    // repeat until the target is within the epsilon of the midpoint
-    // or the max number of iterations is reached
-    } while ((abs(y_target - y_mid) > epsilon) && (max_iter > 0));
-
-    if (max_iter == 0)
-    {
-        cout << "max iter reached. Returning current mid" << endl;
     }
 
-    return mid;
+    // if the maximum number of iterations is reached, return the midpoint
+    // we throw an error instead of returning the midpoint, so that the user can
+    // catch and decide what action to take
+    throw std::runtime_error("The maximum number of iterations was reached");
 }
 
 class BlackScholesCallFunctor
--- Q8/main.cpp
+++ Q8/main.cpp
@@ -13,8 +13,18 @@ int main()
     float call_price = 13.6953;
     int max_iter = 30;
 
+
+    double left = 0.01;
+    double right = 10.0;
+
+    // check that the function has opposite signs at the endpoints
+    if ( (stock(left) - call_price) * (stock(right) - call_price) > 0)
+    {
+        throw std::invalid_argument("The function has the same sign at the left and right endpoints.");
+    }
+
     // calculate implied volatility
-    double impl_vol = interval_bisection<BlackScholesCallFunctor>(call_price, 0.0001, 10.0, 0.0001, stock, max_iter);
+    double impl_vol = interval_bisection<BlackScholesCallFunctor>(call_price, 0.2, 1, 0.0001, stock, max_iter);
     cout << "The Black-Scholes call price is: " << call_price << endl;
     cout << "The implied volatility is: " << impl_vol << endl;
 
